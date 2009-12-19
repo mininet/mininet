15f37cc2924947fc432e465b4ed04852ab9d68f7
Brandon Heller <brandonh@stanford.edu>
2009-12-18 20:03:19 -0800

    Add LinearNet test case

--- mininet/test/test_nets.py
+++ mininet/test/test_nets.py
@@ -7,7 +7,7 @@ Test creation and all-pairs ping for each included mininet topo type.
 from time import sleep
 import unittest
 
-from mininet.mininet import init, TreeNet, pingTest, DATAPATHS
+from mininet.mininet import init, TreeNet, LinearNet, pingTest, DATAPATHS
 
 class testMinimal(unittest.TestCase):
     '''For each datapath type, test ping with a minimal topology.
@@ -16,7 +16,7 @@ class testMinimal(unittest.TestCase):
     '''
 
     def testMinimal(self):
-        '''Ping test with user-space datapath on minimal topology'''
+        '''Ping test with both datapaths on minimal topology'''
         init()
         for datapath in DATAPATHS:
             k = datapath == 'kernel'
@@ -29,13 +29,26 @@ class testTree(unittest.TestCase):
     '''For each datapath type, test all-pairs ping with TreeNet.'''
 
     def testTree16(self):
-        '''Ping test with user-space datapath on minimal topology'''
+        '''Ping test with both datapaths on 16-host topology'''
         init()
         for datapath in DATAPATHS:
             k = datapath == 'kernel'
             network = TreeNet(depth = 2, fanout = 4, kernel = k)
             dropped = network.run(pingTest)
             self.assertEqual(dropped, 0)
+
+
+class testLinear(unittest.TestCase):
+    '''For each datapath type, test all-pairs ping with LinearNet.'''
+
+    def testLinear10(self):
+        '''Ping test  with both datapaths on 10-switch topology'''
+        init()
+        for datapath in DATAPATHS:
+             k = datapath == 'kernel'
+             network = network = LinearNet(10, kernel=k)
+             dropped = network.run(pingTest)
+             self.assertEqual(dropped, 0)
 
 
 if __name__ == '__main__':
