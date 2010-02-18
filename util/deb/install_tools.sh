2b083dd191cb813132d0133654f8a98be975f340
Brandon Heller <brandonh@stanford.edu>
2010-02-18 06:02:00 -0800

    Add gitk to installed VM tools

--- util/deb/install_tools.sh
+++ util/deb/install_tools.sh
@@ -2,5 +2,6 @@
 #Brandon Heller (brandonh@stanford.edu)
 #Install extra tools on Debian Lenny
 
-#Install tcpdump and tshark, cmd-line packet dump tools:
-sudo apt-get install -y tcpdump tshark
+#Install tcpdump and tshark, cmd-line packet dump tools.  Also install gitk,
+#a graphical git history viewer. 
+sudo apt-get install -y tcpdump tshark gitk
