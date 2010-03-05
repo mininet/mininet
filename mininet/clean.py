3eb5abe6a78bc0dd13a26d2013ce72d49ab2750f
Brandon Heller <brandonh@stanford.edu>
2010-03-04 21:47:50 -0800

    Make cleanup a param of mn, not a separate script
    Also switch from print to info statements, to enable a non-verbose
    clean.  Instead of 'mnclean', now run 'mn -c'.

--- mininet/clean.py
+++ mininet/clean.py
@@ -14,17 +14,19 @@ nothing irreplaceable!
 
 from subprocess import Popen, PIPE
 
+from mininet.log import info
 from mininet.xterm import cleanUpScreens
 
 def sh( cmd ):
     "Print a command and send it to the shell"
-    print cmd
+    info( cmd + '\n' )
     return Popen( [ '/bin/sh', '-c', cmd ], stdout=PIPE ).communicate()[ 0 ]
 
 def cleanup():
     """Clean up junk which might be left over from old runs;
        do fast stuff before slow dp and link removal!"""
-    print "*** Removing excess controllers/ofprotocols/ofdatapaths/pings/noxes"
+    info("*** Removing excess controllers/ofprotocols/ofdatapaths/pings/noxes"
+         "\n")
     zombies = 'controller ofprotocol ofdatapath ping nox_core lt-nox_core '
     zombies += 'udpbwtest'
     # Note: real zombie processes can't actually be killed, since they
@@ -32,25 +34,22 @@ def cleanup():
     # you can't connect to them either, so they're mostly harmless.
     sh( 'killall -9 ' + zombies + ' 2> /dev/null' )
 
-    print "*** Removing junk from /tmp"
+    info("*** Removing junk from /tmp\n")
     sh( 'rm -f /tmp/vconn* /tmp/vlogs* /tmp/*.out /tmp/*.log' )
 
-    print "*** Removing old screen sessions"
+    info("*** Removing old screen sessions")
     cleanUpScreens()
 
-    print "*** Removing excess kernel datapaths"
+    info("*** Removing excess kernel datapaths\n")
     dps = sh( "ps ax | egrep -o 'dp[0-9]+' | sed 's/dp/nl:/'" ).split( '\n' )
     for dp in dps:
         if dp != '':
             sh( 'dpctl deldp ' + dp )
 
-    print "*** Removing all links of the pattern foo-ethX"
+    info("*** Removing all links of the pattern foo-ethX\n")
     links = sh( "ip link show | egrep -o '(\w+-eth\w+)'" ).split( '\n' )
     for link in links:
         if link != '':
             sh( "ip link del " + link )
 
-    print "*** Cleanup complete."
-
-if __name__ == "__main__":
-    cleanup()
+    info("*** Cleanup complete.\n")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 scripts = [join('bin', filename) for filename in
-            ['mn', 'mnclean']]
+            ['mn']]
 
 modname = distname = 'mininet'
 
