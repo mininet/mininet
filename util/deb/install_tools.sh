f7f2f977d57a63115b3c3fc2a27a6f2b288264c2
Brandon Heller <brandonh@stanford.edu>
2010-03-04 16:34:12 -0800

    Colorize git output by default in VM

--- util/deb/install_tools.sh
+++ util/deb/install_tools.sh
@@ -5,3 +5,8 @@
 #Install tcpdump and tshark, cmd-line packet dump tools.  Also install gitk,
 #a graphical git history viewer. 
 sudo apt-get install -y tcpdump tshark gitk
+
+#Set git to colorize everything.
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
