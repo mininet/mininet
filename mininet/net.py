3fac5a43d7d2ccde5d544d9070c318e135b76e12
Brandon Heller <brandonh@stanford.edu>
2010-03-13 21:44:11 -0800

    Handle ping output for paused hosts

--- mininet/net.py
+++ mininet/net.py
@@ -383,12 +383,15 @@ class Mininet( object ):
     @staticmethod
     def _parsePing( pingOutput ):
         "Parse ping output and return packets sent, received."
+        # Check for downed link
+        if 'connect: Network is unreachable' in pingOutput:
+            return (1, 0)
         r = r'(\d+) packets transmitted, (\d+) received'
         m = re.search( r, pingOutput )
         if m == None:
             error( '*** Error: could not parse ping output: %s\n' %
                      pingOutput )
-            exit( 1 )
+            return (1, 0)
         sent, received = int( m.group( 1 ) ), int( m.group( 2 ) )
         return sent, received
 
