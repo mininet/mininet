56278434a81819222c1cd026c5759cc23ee77c5e
Bob Lantz <rlantz@cs.stanford.edu>
2010-04-10 21:35:02 -0700

    Changed to use library TreeNet() and remove unnecessary init().

--- examples/tree1024.py
+++ examples/tree1024.py
@@ -9,18 +9,11 @@ and running sysctl -p. Check util/sysctl_addon.
 
 from mininet.cli import CLI
 from mininet.log import setLogLevel
-from mininet.net import init, Mininet
 from mininet.node import KernelSwitch
-from mininet.topolib import TreeTopo
-
-def TreeNet( depth=1, fanout=2, **kwargs ):
-    "Convenience function for creating tree networks."
-    topo = TreeTopo( depth, fanout )
-    return Mininet( topo, **kwargs )
+from mininet.topolib import TreeNet
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-    init()
-    KernelSwitch.setup()
     network = TreeNet( depth=2, fanout=32, switch=KernelSwitch )
     network.run( CLI, network )
+
