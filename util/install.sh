cc16d2ce29621fd777dfc0cd3cedad48966512c5
Bob Lantz <rlantz@cs.stanford.edu>
2010-08-18 23:33:05 -0700

    Attempt to fix OS detection for Debian.

--- util/install.sh
+++ util/install.sh
@@ -11,7 +11,8 @@ set -o nounset
 # Location of CONFIG_NET_NS-enabled kernel(s)
 KERNEL_LOC=http://www.openflow.org/downloads/mininet
 
-DIST=`lsb_release -is`
+test -e /etc/debian_version && DIST="Debian"
+grep Ubuntu /etc/lsb-release &> /dev/null && DIST="Ubuntu"
 
 # Kernel params
 # These kernels have been tried:
@@ -26,6 +27,8 @@ else
     echo "Install.sh currently only supports Ubuntu and Debian."
     exit 1
 fi
+
+echo "Detected Linux distribution: $DIST"
 
 # Kernel Deb pkg to be removed:
 KERNEL_IMAGE_OLD=linux-image-2.6.26-2-686
