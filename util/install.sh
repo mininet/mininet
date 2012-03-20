f85c1cefb4a64a0724aa71f6a114ea572e62434c
Bob Lantz <rlantz@cs.stanford.edu>
2012-03-20 15:49:39 -0700

    Use upstream OVS packages.

--- util/install.sh
+++ util/install.sh
@@ -70,7 +70,7 @@ DRIVERS_DIR=/lib/modules/${KERNEL_NAME}/kernel/drivers/net
 
 OVS_RELEASE=1.4.0
 OVS_PACKAGE_LOC=https://github.com/downloads/mininet/mininet
-OVS_BUILDSUFFIX=-2
+OVS_BUILDSUFFIX=-ignore # was -2
 OVS_PACKAGE_NAME=ovs-$OVS_RELEASE-core-$DIST_LC-$RELEASE-$ARCH$OVS_BUILDSUFFIX.tar
 OVS_SRC=~/openvswitch
 OVS_TAG=v$OVS_RELEASE
