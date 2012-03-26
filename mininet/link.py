2ec866d2c59e6aba6aa82ee127aa0e262b8be002
Bob Lantz <rlantz@cs.stanford.edu>
2012-03-25 20:18:35 -0700

    TCLink: pass correct parameters to superclass.

--- mininet/link.py
+++ mininet/link.py
@@ -387,8 +387,8 @@ class TCLink( Link ):
     "Link with symmetric TC interfaces configured via opts"
     def __init__( self, node1, node2, port1=None, port2=None,
                   intfName1=None, intfName2=None, **params ):
-        Link.__init__( self, node1, node2, port1=None, port2=None,
-                       intfName1=None, intfName2=None,
+        Link.__init__( self, node1, node2, port1=port1, port2=port2,
+                       intfName1=intfName1, intfName2=intfName2,
                        cls1=TCIntf,
                        cls2=TCIntf,
                        params1=params,
