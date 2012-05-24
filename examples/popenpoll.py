e4514a4ecbaf47d6a3b381f1b2d48ab32cfdae27
Bob Lantz <rlantz@cs.stanford.edu>
2012-05-23 21:09:14 -0700

    Still more indentation errors. ;-p

--- examples/popenpoll.py
+++ examples/popenpoll.py
@@ -24,9 +24,9 @@ def pmonitorTest( N=3, seconds=10 ):
     for h, line in pmonitor( popens, timeoutms=500 ):
        if h:
           print '%s: %s' % ( h.name, line ),
-          if time() >= endTime:
-             for p in popens.values():
-                p.send_signal( SIGINT )
+       if time() >= endTime:
+          for p in popens.values():
+             p.send_signal( SIGINT )
     net.stop()
 
 if __name__ == '__main__':
