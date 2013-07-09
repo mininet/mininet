60abb344974217362d0847572df43da8ced0fece
Rich Lane <rlane@bigswitch.com>
2013-07-09 08:36:36 -0700

    clean: kill ivs processes

--- mininet/clean.py
+++ mininet/clean.py
@@ -27,7 +27,7 @@ def cleanup():
     info("*** Removing excess controllers/ofprotocols/ofdatapaths/pings/noxes"
          "\n")
     zombies = 'controller ofprotocol ofdatapath ping nox_core lt-nox_core '
-    zombies += 'ovs-openflowd ovs-controller udpbwtest mnexec'
+    zombies += 'ovs-openflowd ovs-controller udpbwtest mnexec ivs'
     # Note: real zombie processes can't actually be killed, since they
     # are already (un)dead. Then again,
     # you can't connect to them either, so they're mostly harmless.
