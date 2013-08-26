c353e6091380d48c0388565e3b08a327b3146a92
Bob Lantz <rlantz@cs.stanford.edu>
2013-08-25 18:06:22 -0700

    correction: pexpect.timeout -> TIMEOUT

--- util/vm/build.py
+++ util/vm/build.py
@@ -427,7 +427,7 @@ def interact( vm ):
     # know the time for each test, which means that this
     # script will have to change as we add more tests.
     for test in range( 0, 2 ):
-        if vm.expect( [ 'OK', 'FAILED', pexpect.timeout ], timeout=60 ) == 0:
+        if vm.expect( [ 'OK', 'FAILED', pexpect.TIMEOUT ], timeout=60 ) == 0:
             log( '* Test', test, 'OK' )
         else:
             log( '* Test', test, 'FAILED' )
