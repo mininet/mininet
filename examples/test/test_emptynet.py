49fc496c122e2fe49272dddbf490d328a6641ae3
Brian O'Connor <bocon13@gmail.com>
2013-09-10 14:56:51 -0700

    cleaned up and commented test_emptynet.py

--- examples/test/test_emptynet.py
+++ examples/test/test_emptynet.py
@@ -1,39 +1,32 @@
 #!/usr/bin/env python
 
-"""TEST"""
+"""
+Test for emptynet.py
+"""
 
 import unittest
 import pexpect
-#from time import sleep
-from mininet.log import setLogLevel
-#from mininet.net import Mininet
-#from mininet.node import CPULimitedHost
-#from mininet.link import TCLink
-
-#from mininet.examples.simpleperf import SingleSwitchTopo
 
 class testEmptyNet( unittest.TestCase ):
-    "Test ping with single switch topology (common code)."
 
     prompt = 'mininet>'
 
     def testEmptyNet( self ):
+        "Run simple CLI tests: pingall (verify 0% drop) and iperf (sanity)"
         p = pexpect.spawn( 'python -m mininet.examples.emptynet' )
         p.expect( self.prompt )
+        # pingall test
         p.sendline( 'pingall' )
         p.expect ( '(\d+)% dropped' )
         percent = int( p.match.group( 1 ) ) if p.match else -1
-        self.assertEqual( percent, 0 ) # or this
+        self.assertEqual( percent, 0 ) 
         p.expect( self.prompt )
+        # iperf test
         p.sendline( 'iperf' )
         p.expect( "Results: \['[\d.]+ .bits/sec', '[\d.]+ .bits/sec'\]" )
-        #TODO check the results? maybe we dont care
         p.expect( self.prompt )
         p.sendline( 'exit' )
         p.wait()
-        #TODO remove this
-        self.assertEqual( percent, 0 )
 
 if __name__ == '__main__':
-    setLogLevel( 'warning' )
     unittest.main()
