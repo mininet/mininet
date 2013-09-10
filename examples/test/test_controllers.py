fba3fd81fa21877aed1eda9ee0e98655d96b404a
Brian O'Connor <bocon13@gmail.com>
2013-09-10 14:37:01 -0700

    cleaned up and commented test_controllers.py

--- examples/test/test_controllers.py
+++ examples/test/test_controllers.py
@@ -1,41 +1,36 @@
 #!/usr/bin/env python
 
-"""TEST"""
+"""
+Tests for controllers.py and controllers2.py
+"""
 
 import unittest
 import pexpect
-#from time import sleep
-from mininet.log import setLogLevel
-#from mininet.net import Mininet
-#from mininet.node import CPULimitedHost
-#from mininet.link import TCLink
-
-#from mininet.examples.simpleperf import SingleSwitchTopo
 
 class testControllers( unittest.TestCase ):
-    "Test ping with single switch topology (common code)."
 
     prompt = 'mininet>'
 
     def connectedTest( self, name, cmap ):
+        "Verify that switches are connected to the controller specified by cmap"
         p = pexpect.spawn( 'python -m %s' % name )
         p.expect( self.prompt )
+        # but first a simple ping test
         p.sendline( 'pingall' )
         p.expect ( '(\d+)% dropped' )
         percent = int( p.match.group( 1 ) ) if p.match else -1
-        self.assertEqual( percent, 0 ) # or this
+        self.assertEqual( percent, 0 )
         p.expect( self.prompt )
+        # verify connected controller
         for switch in cmap:
             p.sendline( 'sh ovs-vsctl get-controller %s' % switch )
             p.expect( 'tcp:([\d.:]+)')
             actual = p.match.group(1)
             expected = cmap[ switch ]
-            self.assertEqual( actual, expected)
+            self.assertEqual( actual, expected )
         p.expect( self.prompt )
         p.sendline( 'exit' )
         p.wait()
-        #TODO remove this
-        self.assertEqual( percent, 0 )
 
     def testControllers( self ):
         c0 = '127.0.0.1:6633'
@@ -49,7 +44,5 @@ class testControllers( unittest.TestCase ):
         cmap = { 's1': c0, 's2': c1 }
         self.connectedTest( 'mininet.examples.controllers2', cmap )
 
-
 if __name__ == '__main__':
-    setLogLevel( 'warning' )
     unittest.main()
