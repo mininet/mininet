d4993c0ba4c8232aa9d37903da1c95f4ab897330
Brian O'Connor <bocon13@gmail.com>
2013-09-10 16:20:44 -0700

    cleaned up and commented test_scratchnet.py

--- examples/test/test_scratchnet.py
+++ examples/test/test_scratchnet.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 
-"""TEST"""
+"""
+Test for scratchnet.py
+"""
 
 import unittest
 import pexpect
-from mininet.log import setLogLevel
 
 class testScratchNet( unittest.TestCase ):
-    "Test ping with single switch topology (common code)."
 
-    results = [ "1 packets transmitted, 1 received, 0% packet loss", pexpect.EOF ]
+    opts = [ "1 packets transmitted, 1 received, 0% packet loss", pexpect.EOF ]
 
     def pingTest( self, name ):
+        "Verify that no ping packets were dropped"
         p = pexpect.spawn( 'python -m %s' % name )
-        index = p.expect( self.results )
+        index = p.expect( self.opts )
         self.assertEqual( index, 0 )
-
 
     def testPingKernel( self ):
         self.pingTest( 'mininet.examples.scratchnet' )
-
 
     def testPingUser( self ):
         self.pingTest( 'mininet.examples.scratchnetuser' )
 
 if __name__ == '__main__':
-    setLogLevel( 'warning' )
     unittest.main()
