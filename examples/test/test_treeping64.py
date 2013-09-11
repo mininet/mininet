3577a6989d2bbb0e5c7664e693968267a63ac0a4
Brian O'Connor <bocon13@gmail.com>
2013-09-10 17:14:31 -0700

    cleaned up and commented test_tree1024.py and test_treeping64.py

--- examples/test/test_treeping64.py
+++ examples/test/test_treeping64.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 
-"""TEST"""
+"""
+Test for treeping64.py
+"""
 
 import unittest
 import pexpect
-#from time import sleep
-from mininet.log import setLogLevel
-#from mininet.net import Mininet
-#from mininet.node import CPULimitedHost
-#from mininet.link import TCLink
-
-#from mininet.examples.simpleperf import SingleSwitchTopo
 
 class testTreePing64( unittest.TestCase ):
-    "Test ping with single switch topology (common code)."
 
     prompt = 'mininet>'
 
     def testTreePing64( self ):
+        "Run the example and verify ping results"
         p = pexpect.spawn( 'python -m mininet.examples.treeping64' )
         p.expect( 'Tree network ping results:', timeout=6000 )
         count = 0
@@ -32,5 +27,4 @@ class testTreePing64( unittest.TestCase ):
         self.assertTrue( count > 0 )
 
 if __name__ == '__main__':
-    setLogLevel( 'warning' )
     unittest.main()
