7eeaed992c3da32684a0ebd712baa0aad27d5137
cody burkard <cody@onlab.us>
2014-09-25 12:01:06 -0700

    use udp with iperf to measure loss. pings are not reliable

--- examples/test/test_simpleperf.py
+++ examples/test/test_simpleperf.py
@@ -6,12 +6,8 @@ Test for simpleperf.py
 
 import unittest
 import pexpect
-import re
 import sys
 from mininet.log import setLogLevel
-from mininet.net import Mininet
-from mininet.node import CPULimitedHost
-from mininet.link import TCLink
 
 from mininet.examples.simpleperf import SingleSwitchTopo
 
@@ -19,34 +15,19 @@ class testSimplePerf( unittest.TestCase ):
 
     @unittest.skipIf( '-quick' in sys.argv, 'long test' )
     def testE2E( self ):
-        "Run the example and verify ping and iperf results"
+        "Run the example and verify iperf results"
+        BW = 10
+        TOLERANCE = .8
+        expectedBw = BW * TOLERANCE
         p = pexpect.spawn( 'python -m mininet.examples.simpleperf' )
-        # check ping results
-        p.expect( "Results: (\d+)% dropped", timeout=120 )
-        loss = int( p.match.group( 1 ) )
-        self.assertTrue( 0 < loss < 100 )
         # check iperf results
-        p.expect( "Results: \['([\d\.]+) .bits/sec", timeout=480 )
-        bw = float( p.match.group( 1 ) )
-        self.assertTrue( bw > 0 )
+        p.expect( "Results: \['10M', '([\d\.]+) .bits/sec", timeout=480 )
+        measuredBw = float( p.match.group( 1 ) )
+        lowerBound = expectedBw * TOLERANCE
+        upperBound = expectedBw + expectedBw * ( 1 - TOLERANCE )
+        self.assertGreaterEqual( measuredBw, lowerBound )
+        self.assertLessEqual( measuredBw, upperBound )
         p.wait()
-
-    def testTopo( self ):
-        """Import SingleSwitchTopo from example and test connectivity between two hosts
-           Note: this test may fail very rarely because it is non-deterministic
-           i.e. links are configured with 10% packet loss, but if we get unlucky and 
-           none or all of the packets are dropped, the test will fail"""
-        topo = SingleSwitchTopo( n=4 )
-        net = Mininet( topo=topo, host=CPULimitedHost, link=TCLink )
-        net.start()
-        h1, h4 = net.get( 'h1', 'h4' )
-        # have h1 ping h4 ten times
-        expectStr = '(\d+) packets transmitted, (\d+) received, (\d+)% packet loss'
-        output = h1.cmd( 'ping -c 10 %s' % h4.IP() )
-        m = re.search( expectStr, output )
-        loss = int( m.group( 3 ) )
-        net.stop()
-        self.assertTrue( 0 < loss < 100 )
 
 if __name__ == '__main__':
     setLogLevel( 'warning' )
