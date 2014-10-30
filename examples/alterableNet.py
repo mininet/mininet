05a9d46f341463485c1ee93c769f9668df0a87a8
Jon Hall <Jhall@onlab.us>
2014-10-30 13:53:06 -0700

    Initial attempt at adding custom topo to alterable cli

--- examples/alterableNet.py
+++ examples/alterableNet.py
@@ -11,6 +11,59 @@ from mininet.topo import SingleSwitchTopo
 from mininet.cli import CLI
 from mininet.net import Mininet
 from mininet.log import setLogLevel, info, error, debug
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+	"Basic topo used for nightly testing."
+
+	def __init__( self ):
+		"Create custom topo."
+		# Initialize topology
+		Topo.__init__( self )
+
+		# Make the middle triangle	
+		leftSwitch = self.addSwitch( 's1' , dpid = '1000'.zfill(16))
+		rightSwitch = self.addSwitch( 's2' , dpid = '2000'.zfill(16))
+		topSwitch = self.addSwitch( 's3' , dpid = '3000'.zfill(16))
+		lefthost = self.addHost( 'h1' )
+		righthost = self.addHost( 'h2' )
+		tophost = self.addHost( 'h3' )
+		self.addLink( leftSwitch, lefthost )
+		self.addLink( rightSwitch, righthost )
+		self.addLink( topSwitch, tophost )
+
+		self.addLink( leftSwitch, rightSwitch )
+		self.addLink( leftSwitch, topSwitch )
+		self.addLink( topSwitch, rightSwitch )
+
+		# Make aggregation switches
+		agg1Switch = self.addSwitch( 's4', dpid = '1004'.zfill(16) ) 
+		agg2Switch = self.addSwitch( 's5', dpid = '2005'.zfill(16) ) 
+		agg1Host = self.addHost( 'h4' ) 
+		agg2Host = self.addHost( 'h5' ) 
+
+		self.addLink( agg1Switch, agg1Host, port1=1, port2=1 )
+		self.addLink( agg2Switch, agg2Host, port1=1, port2=1 )
+
+		self.addLink( agg2Switch, rightSwitch )
+		self.addLink( agg1Switch, leftSwitch )
+
+		# Make two aggregation fans
+		for i in range(10):
+			num=str(i+6)
+			switch = self.addSwitch( 's' + num, dpid = ('10' + num.zfill(2) ).zfill(16))
+			host = self.addHost( 'h' + num ) 
+			self.addLink( switch, host, port1=1, port2=1 ) 
+			self.addLink( switch, agg1Switch ) 
+
+		for i in range(10):
+			num=str(i+31)
+			switch = self.addSwitch( 's' + num, dpid = ('20' + num.zfill(2)).zfill(16) )
+			host = self.addHost( 'h' + num ) 
+			self.addLink( switch, host, port1=1, port2=1 ) 
+			self.addLink( switch, agg2Switch ) 
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
 
 class alterableCLI( CLI ):
     "allow addition and deletion of objects from CLI"
@@ -142,6 +195,7 @@ class alterNet( Mininet ):
         """remove a node from a running network
            returns True if successful"""
         if node not in self.hosts + self.switches:
+            #NOTE: what if node is a controller?
             error( '%s doesnt exist!\n' % node )
             return 0
         node.stop()
@@ -190,7 +244,8 @@ class alterNet( Mininet ):
 
 
 def testCLI():
-    net = alterNet(topo = SingleSwitchTopo())
+    net = alterNet(topo = MyTopo())
+    print type(net)
     net.start()
     alterableCLI( net )
     net.stop()
