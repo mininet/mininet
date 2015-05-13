63ae13fcf95a3e66357ef1671e77030681d44321
Bob Lantz <rlantz@cs.stanford.edu>
2015-05-12 23:50:37 -0700

    Fix #520

--- mininet/node.py
+++ mininet/node.py
@@ -1143,7 +1143,7 @@ class OVSSwitch( Switch ):
         if self.protocols and not self.isOldOVS():
             opts += ' protocols=%s' % self.protocols
         if self.stp and self.failMode == 'standalone':
-            opts += ' stp_enable=true' % self
+            opts += ' stp_enable=true'
         return opts
 
     def start( self, controllers ):
