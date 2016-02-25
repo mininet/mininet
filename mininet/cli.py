ac4a673e6f5e2dd1d664c7e366f4a0fbd24b2bd9
Jason Croft <croftbc@gmail.com>
2016-02-25 03:50:22 -0600

    Set limit on command history length

--- mininet/cli.py
+++ mininet/cli.py
@@ -77,13 +77,15 @@ class CLI( Cmd ):
             return
         cls.readlineInited = True
         try:
-            from readline import read_history_file, write_history_file
+            from readline import ( read_history_file, write_history_file,
+                                   set_history_length )
         except ImportError:
             pass
         else:
             history_path = os.path.expanduser( '~/.mininet_history' )
             if os.path.isfile( history_path ):
                 read_history_file( history_path )
+                set_history_length( 1000 )
             atexit.register( lambda: write_history_file( history_path ) )
 
     def run( self ):
