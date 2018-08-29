1f4525ed0affb0eed7b3091aa775db0f2efa7b1e
Bob Lantz <rlantz@cs.stanford.edu>
2018-08-28 18:43:03 -0700

    Test for Node()/Host()  pty leakage

---
+++ mininet/test/test_ptyleak.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Regression test for pty leak in Node()
+"""
+
+import unittest
+
+from mininet.net import Mininet
+from mininet.clean import cleanup
+from mininet.topo import SingleSwitchTopo
+
+class TestPtyLeak( unittest.TestCase ):
+    "Verify that there is no pty leakage"
+
+    @staticmethod
+    def testPtyLeak():
+        "Test for pty leakage"
+        net = Mininet( SingleSwitchTopo() )
+        net.start()
+        host = net[ 'h1' ]
+        for _ in range( 0, 10 ):
+            oldptys = host.slave, host.master
+            net.delHost( host )
+            host = net.addHost( 'h1' )
+            assert ( host.slave, host.master ) == oldptys
+        net.stop()
+
+if __name__ == '__main__':
+    unittest.main()
+    cleanup()
