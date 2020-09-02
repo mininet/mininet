f78627513b86d4a347e3b228af831b8a025db138
Alex Lane <awlane@memphis.edu>
2020-09-01 21:38:24 -0500

    Fix to allow loss to be set to 0% on TCIntf using config

--- mininet/link.py
+++ mininet/link.py
@@ -293,7 +293,7 @@ class TCIntf( Intf ):
         else:
             # Delay/jitter/loss/max queue size
             netemargs = '%s%s%s%s' % (
-                'delay %s ' % delay if delay is not None else '',
+                'delay %s ' % delay if (delay is not None and delay > 0) else '',
                 '%s ' % jitter if jitter is not None else '',
                 'loss %.5f ' % loss if (loss is not None and loss > 0) else '',
                 'limit %d' % max_queue_size if max_queue_size is not None
@@ -349,7 +349,7 @@ class TCIntf( Intf ):
 
         # Optimization: return if nothing else to configure
         # Question: what happens if we want to reset things?
-        if ( bw is None and not delay and not loss
+        if ( bw is None and delay is None and loss is None
              and max_queue_size is None ):
             return
 
@@ -359,6 +359,7 @@ class TCIntf( Intf ):
             cmds = [ '%s qdisc del dev %s root' ]
         else:
             cmds = []
+
 
         # Bandwidth limits via various methods
         bwcmds, parent = self.bwCmds( bw=bw, speedup=speedup,
