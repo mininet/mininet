0a06c7a8a58432e36113f8d8e72adfda1bdd541c
jbmonte <john.montecastro@students.mq.edu.au>
2020-09-08 13:54:49 +1000

    Create test.py

---
+++ custom/test.py
@@ -0,0 +1,23 @@
+from mininet.topo import Topo  
+class MyTopo( Topo ):  
+    "Simple topology example."
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        Host1 = self.addHost( 'h1' )
+        Host2 = self.addHost( 'h2' )
+        Host3 = self.addHost( 'h3' )
+        Host4 = self.addHost( 'h4' )
+        Switch1 = self.addSwitch('s1')
+        Switch2 = self.addSwitch('s2')
+        # Add links
+        self.addLink( Host1, Switch1 )
+        self.addLink( Host2, Switch1 )
+        self.addLink( Host3, Switch2 )
+        self.addLink( Host4, Switch2 )
+        self.addLink( Switch1, Switch2 )
+topos = { 'mytopo': ( lambda: MyTopo() ) } 
