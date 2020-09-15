691bfae253276b6492012718c0b1fa4f7c729890
Will Fantom <willfantom@gmail.com>
2020-09-15 16:36:42 +0100

    Support python api usage in container

--- util/docker/entrypoint.sh
+++ util/docker/entrypoint.sh
@@ -3,7 +3,26 @@
 service openvswitch-switch start
 ovs-vsctl set-manager ptcp:6640
 
-sudo mn -c
-sudo mn $MN_FLAGS
+function clean_exit {
+  service openvswitch-switch stop
+  echo "*** Exiting Container..."
+  exit 0
+}
 
-service openvswitch-switch stop
+sudo mn -c
+
+if [ -z "$FILE_PATH" ]
+then
+  # Run using mn if no file path given
+  sudo mn $MN_FLAGS
+else
+  # Use python api if a file path is given
+  if [ ! -f "$FILE_PATH" ]
+  then
+    echo "*** Error: given file does not exist in container's filesystem"
+    clean_exit
+  fi
+  sudo python3 $FILE_PATH
+fi
+
+clean_exit
