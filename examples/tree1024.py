9786187c7525307d6980f1195a4542717566cef8
Bob Lantz <rlantz@cs.stanford.edu>
2021-01-04 13:41:21 -0800

    Fix tree1024.py

--- examples/tree1024.py
+++ examples/tree1024.py
@@ -9,13 +9,12 @@ and running sysctl -p. Check util/sysctl_addon.
 
 from mininet.cli import CLI
 from mininet.log import setLogLevel
-from mininet.node import OVSSwitch, Host
+from mininet.node import OVSSwitch
 from mininet.topolib import TreeNet
-from mininet.examples.treeping64 import HostV4, QuietController
+from mininet.examples.treeping64 import HostV4
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
     network = TreeNet( depth=2, fanout=32, host=HostV4,
-                       switch=OVSSwitch, controller=QuietController,
-                       waitConnected=True)
+                       switch=OVSSwitch, waitConnected=True)
     network.run( CLI, network )
