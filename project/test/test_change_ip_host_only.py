0899c52fd77d9d40363d9789876fb601b42a152a
None <None>
2021-06-08 20:40:56 -0700

    Added test host only basic unit test pingAllHostOnlyInf

--- project/test/test_change_ip_host_only.py
+++ project/test/test_change_ip_host_only.py
@@ -9,7 +9,52 @@ sys.path.append('..')
 
 from custom_node import HostConnectedNode
 from mininet.net import Mininet
+from mininet.log import error, output
 from mininet.log import setLogLevel
+
+
+def pingHostOnlyInf(self, hosts=None, timeout=None):
+    """Ping between all specified hosts.
+       hosts: list of hosts
+       timeout: time to wait for a response, as string
+       returns: ploss packet loss percentage"""
+    # should we check if running?
+    packets = 0
+    lost = 0
+    ploss = None
+    if not hosts:
+        hosts = self.hosts
+        output('*** Ping: testing ping reachability\n')
+    for node in hosts:
+        output('%s -> ' % node.name)
+        for dest in hosts:
+            if node != dest:
+                opts = ''
+                if timeout:
+                    opts = '-W %s' % timeout
+                if dest.intfs:
+                    result = node.cmd(f"ping -c1 {opts} -I {node.getHostOnlyIntf()} {dest.IP(intf=dest.getHostOnlyIntf())}")
+                    sent, received = self._parsePing(result)
+                else:
+                    sent, received = 0, 0
+                packets += sent
+                if received > sent:
+                    error('*** Error: received too many packets')
+                    error('%s' % result)
+                    node.cmdPrint('route')
+                    exit(1)
+                lost += sent - received
+                output(('%s ' % dest.name) if received else 'X ')
+        output('\n')
+    if packets > 0:
+        ploss = 100.0 * lost / packets
+        received = packets - lost
+        output("*** Results: %i%% dropped (%d/%d received)\n" %
+               (ploss, received, packets))
+    else:
+        ploss = 0
+        output("*** Warning: No packets sent\n")
+    return ploss
 
 
 class Test(unittest.TestCase):
@@ -24,13 +69,13 @@ class Test(unittest.TestCase):
         mn.addLink(h2, s1)
 
         mn.start()
-
+        dropped = mn.pingAll()
         h2.config(ip="10.0.0.20")
 
-        dropped = mn.pingAll()
+        dropped_host_only = pingHostOnlyInf()
 
         self.assertEqual(dropped, 0)
-
+        self.assertEqual(dropped_host_only, 0)
         mn.stop()
 
 
--- project/test/test_host_only.py
+++ project/test/test_host_only.py
@@ -11,7 +11,52 @@ from custom_node import HostConnectedNode
 from mininet.net import Mininet
 from mininet.node import Host
 from mininet.clean import cleanup
+from mininet.log import info, error, debug, output, warn
 from mininet.log import setLogLevel
+
+
+def pingHostOnlyInf(self, hosts=None, timeout=None):
+    """Ping between all specified hosts.
+       hosts: list of hosts
+       timeout: time to wait for a response, as string
+       returns: ploss packet loss percentage"""
+    # should we check if running?
+    packets = 0
+    lost = 0
+    ploss = None
+    if not hosts:
+        hosts = self.hosts
+        output('*** Ping: testing ping reachability\n')
+    for node in hosts:
+        output('%s -> ' % node.name)
+        for dest in hosts:
+            if node != dest:
+                opts = ''
+                if timeout:
+                    opts = '-W %s' % timeout
+                if dest.intfs:
+                    result = node.cmd(f"ping -c1 {opts} -I {node.getHostOnlyIntf()} {dest.IP(intf=dest.getHostOnlyIntf())}")
+                    sent, received = self._parsePing(result)
+                else:
+                    sent, received = 0, 0
+                packets += sent
+                if received > sent:
+                    error('*** Error: received too many packets')
+                    error('%s' % result)
+                    node.cmdPrint('route')
+                    exit(1)
+                lost += sent - received
+                output(('%s ' % dest.name) if received else 'X ')
+        output('\n')
+    if packets > 0:
+        ploss = 100.0 * lost / packets
+        received = packets - lost
+        output("*** Results: %i%% dropped (%d/%d received)\n" %
+               (ploss, received, packets))
+    else:
+        ploss = 0
+        output("*** Warning: No packets sent\n")
+    return ploss
 
 
 class Test(unittest.TestCase):
